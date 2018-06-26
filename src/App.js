79b47a2128e5ca091ab847bc45235c14070b96c9
Meina <meinsta@gmail.com>
2018-06-25 20:30:26 -0700

    begin to style page, and beginnings of carousel component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
 
 class App extends Component {
   render() {
@@ -8,11 +10,17 @@ class App extends Component {
         <video autoPlay playsInline muted loop id="hero"><source src="https://meinsta.github.io/auth0-hpg/Hero/atom.mp4" type="video/mp4" /></video>
         <aside>
         <h1>Never compromise on <span className="emphasis">identity</span></h1>
-        <caption>We provide a universal authentication &amp; authorization platform for web, mobile and legacy applications.</caption>
+        <caption className="caption-hero">We provide a universal authentication &amp; authorization platform for web, mobile and legacy applications.</caption>
         </aside>
       </header>
       <main>
-        Main
+        <section className="carousel">
+          <div className="carousel-container">
+            <h2>What’s Auth0</h2>
+            <caption className="caption-carousel">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</caption>
+            <videoCarousel />
+          </div>
+        </section>
       </main>
       <footer>
       <section className="customers">
@@ -25,6 +33,35 @@ class App extends Component {
       </div>
     );
   }
+}
+
+class videoCarousel extends Component {
+    render() {
+        return (
+            <Carousel>
+                <div>
+                    <video><sourc src="https://meinsta.github.io/auth0-bg/What_is_Auth0/01_Use_Cases.mp4" type="video/mp4" /></video>
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <video><sourc src="https://meinsta.github.io/auth0-bg/What_is_Auth0/02_Technology_1.mp4" type="video/mp4" /></video>
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <video><sourc src="https://meinsta.github.io/auth0-bg/What_is_Auth0/03_Deployment.mp4" type="video/mp4" /></video>
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <video><sourc src="https://meinsta.github.io/auth0-bg/What_is_Auth0/04_Customization.mp4" type="video/mp4" /></video>
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <video><sourc src="https://meinsta.github.io/auth0-bg/What_is_Auth0/04_Result.mp4" type="video/mp4" /></video>
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        );
+    }
 }
 
 export default App;
