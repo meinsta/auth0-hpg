019cc0a8e56165b0938152c5582c22950751188d
Meina <meinsta@gmail.com>
2018-07-03 16:38:06 -0700

    update wrong tag for an H3 in cloud component

--- src/components/CustomerCloud.js
+++ src/components/CustomerCloud.js
@@ -14,7 +14,7 @@ export default class CustomerCloud extends React.Component {
           <section className="customers">
             <h2 className="h2-customers">Join thousands of companies that trust Auth0 everyday</h2>
             <div className="cloud-container">{ svgs }</div>
-            <p className="h3-link">See All Customers <div className="triangle" /></p>
+            <h3 className="h3-link">See All Customers <div className="triangle" /></h3>
           </section>
         );
     }
