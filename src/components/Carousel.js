b1c5285f56a6f709f59fb336de19836590158e7d
Meina <meinsta@gmail.com>
2018-07-05 19:29:03 -0700

    create promise to resolve remove event listener error

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,8 +11,7 @@ const sources = [
   { id: 4, name: 'Result', class: 'result', video: 'https://meinsta.github.io/auth0-hpg/What_is_Auth0/05_Result.mp4', image: 'https://meinsta.github.io/auth0-hpg/What_is_Auth0/svg/Result.svg' }
 ];
 
-var navListClass = 'slide-list-nav-item',
-	navListItemName = 'dot';
+var navListClass = 'slide-list-nav-item';
 
 var lastIndex = sources.length -1;
 
@@ -22,7 +21,7 @@ export default class Carousel extends React.Component {
 		
 		this.state = {
 			currentIndex: 0,
-			videoVisibility: true
+			videoVisibility: true, 
 		};
 
 		this.videoHandler = this.videoHandler.bind(this);
@@ -42,22 +41,23 @@ export default class Carousel extends React.Component {
 		var videoElement = document.querySelector('.new-video');
 		if(!videoElement) { return; }
 		if(this.state.currentIndex === lastIndex) {
-			this.setState({
-				currentIndex: lastIndex,
-				videoVisibility: false
+			var that = this;
+			return new Promise(function (resolve) {
+			    document.querySelector('.new-carousel video').addEventListener('ended', function endedListener() {
+			        
+			    	document.querySelector('.new-carousel video').removeEventListener('ended', endedListener);
+	            	that.setState({
+						currentIndex: lastIndex,
+						videoVisibility: false
+					});
+			        resolve();
+			    });
 			});
-			document.querySelector('.new-carousel video').removeEventListener('ended', this.videoHandler, false);
-
 		}
 		var updatedIndex = this.state.currentIndex + 1;
 		this.setState({
 			currentIndex: updatedIndex
 		});
-
-	    if(this.state.currentIndex === event.target.id && this.state.currentIndex !== sources.length -1 ) {
-	    	navListClass = 'slide-list-nav-item selected';
-	    	navListItemName = 'dot';
-	    }
 	}
 
 	changeSource(event) {
@@ -72,20 +72,6 @@ export default class Carousel extends React.Component {
 		this.setState({
 			currentIndex: event.target.id
 		});
-
-	    if(this.state.currentIndex === event.target.id && this.state.currentIndex !== lastIndex ) {
-	    	navListClass = 'slide-list-nav-item selected';
-	    	navListItemName = 'dot';
-	    } else if(this.state.currentIndex === lastIndex) {
-	    	navListClass = 'slide-list-nav-item completed';
-	    	navListItemName = 'dot' + ' ' + sources[this.state.currentIndex].class;
-	    }
-	    else {
-   	    	navListClass = 'slide-list-nav-item';
-	    	navListItemName = 'dot'; 	
-	    }
-		document.querySelector('.new-carousel video').removeEventListener('ended', this.videoHandler, false);
-
 	}
 
 	render () {
@@ -95,11 +81,11 @@ export default class Carousel extends React.Component {
 					<Video url={ sources[this.state.currentIndex].video } />
 					<div className="slide-list-nav">
 			          <div className="slide-list-nav-wrapper">
-			            <Button id={0} onClick={ this.changeSource } className={ this.state.currentIndex === 0 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }><div className="dot" />Use Cases</Button>
-			            <Button id={1} onClick={ this.changeSource } className={ this.state.currentIndex === 1 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }><div className="dot" />Technology</Button>
-			            <Button id={2} onClick={ this.changeSource } className={ this.state.currentIndex === 2 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }><div className="dot" />Deployment</Button>
-			            <Button id={3} onClick={ this.changeSource } className={ this.state.currentIndex === 3 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }><div className="dot" />Customization</Button>
-			            <Button id={4} onClick={ this.changeSource } className={ this.state.currentIndex === 4 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }><div className="dot" />Result</Button>
+			            <Button id={0} onClick={ this.changeSource } className={ this.state.currentIndex === 0 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }>Use Cases</Button>
+			            <Button id={1} onClick={ this.changeSource } className={ this.state.currentIndex === 1 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }>Technology</Button>
+			            <Button id={2} onClick={ this.changeSource } className={ this.state.currentIndex === 2 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }>Deployment</Button>
+			            <Button id={3} onClick={ this.changeSource } className={ this.state.currentIndex === 3 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }>Customization</Button>
+			            <Button id={4} onClick={ this.changeSource } className={ this.state.currentIndex === 4 ? 'slide-list-nav-item selected': 'slide-list-nav-item' }>Result</Button>
 			          </div>
 			        </div>
 				</div>
@@ -111,11 +97,11 @@ export default class Carousel extends React.Component {
 					<Image url={ sources[this.state.currentIndex].image } />
 					<div className="slide-list-nav">
 			          <div className="slide-list-nav-wrapper">
-			            <Button id={0} onClick={ this.changeImage } className={navListClass}><div className={navListItemName} />Use Cases</Button>
-			            <Button id={1} onClick={ this.changeImage } className={navListClass}><div className={navListItemName} />Technology</Button>
-			            <Button id={2} onClick={ this.changeImage } className={navListClass}><div className={navListItemName} />Deployment</Button>
-			            <Button id={3} onClick={ this.changeImage } className={navListClass}><div className={navListItemName} />Customization</Button>
-			            <Button id={4} onClick={ this.changeImage } className={navListClass}><div className={navListItemName} />Result</Button>
+			            <Button id={0} onClick={ this.changeImage } className={navListClass}>Use Cases</Button>
+			            <Button id={1} onClick={ this.changeImage } className={navListClass}>Technology</Button>
+			            <Button id={2} onClick={ this.changeImage } className={navListClass}>Deployment</Button>
+			            <Button id={3} onClick={ this.changeImage } className={navListClass}>Customization</Button>
+			            <Button id={4} onClick={ this.changeImage } className={navListClass}>Result</Button>
 			          </div>
 			        </div>
 				</div>
